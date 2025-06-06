8f664fcc4c39338fdac666673566d7faa2cf55f6
Rijan <rijan4568@gmail.com>
2025-06-06 22:29:49 +0545

    feat: dummy data for category

---
+++ lib/data/dummy_categories.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter_application_1/models/category_model.dart';
+
+final List<CategoryModel> dummyCategories = [
+  CategoryModel(label: 'Hoodies', image: 'assets/images/first.png'),
+  CategoryModel(label: 'Shorts', image: 'assets/images/first.png'),
+  CategoryModel(label: 'Shoes', image: 'assets/images/first.png'),
+  CategoryModel(label: 'Bag', image: 'assets/images/first.png'),
+  CategoryModel(label: 'Accessories', image: 'assets/images/first.png'),
+];
