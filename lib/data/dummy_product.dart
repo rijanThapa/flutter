3d0de8ea9303c1a5a13eee47c19a1b08fb4b595b
Rijan <rijan4568@gmail.com>
2025-06-06 22:29:59 +0545

    feat: dummy data for product

---
+++ lib/data/dummy_product.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_application_1/models/product.model.dart';
+
+final List<ProductModel> dummyProducts = [
+  ProductModel(
+    label: 'Hoodies',
+    image: 'assets/images/second.png',
+    price: '\$45000',
+  ),
+  ProductModel(
+    label: 'Shorts',
+    image: 'assets/images/second.png',
+    price: '\$300000',
+  ),
+  ProductModel(
+    label: 'Shoes',
+    image: 'assets/images/second.png',
+    price: '\$60000',
+  ),
+  ProductModel(
+    label: 'Bag',
+    image: 'assets/images/second.png',
+    price: '\$5000',
+  ),
+  ProductModel(
+    label: 'Accessories',
+    image: 'assets/images/second.png',
+    price: '\$2000',
+  ),
+];
